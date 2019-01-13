5b566027bdc3d49e5ad1d907f733a56e15860d2e
Tedris <vanloone@gmail.com>
2019-01-13 16:01:24 -0500

    Fixing build errors and changing things around

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+//import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
 
@@ -35,7 +35,6 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -62,7 +61,7 @@ const Navbar = class extends React.Component {
       <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/Tedris/gatsby-starter-netlify-cms"
           target="_blank"
           rel="noopener noreferrer"
         >
