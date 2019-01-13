376f573934cbd87c3c9232bf310255e270449983
Tedris <vanloone@gmail.com>
2019-01-13 16:31:14 -0500

    Adding back product page

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -7,6 +7,7 @@ import Testimonials from '../components/Testimonials'
 import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
+
 export const ProductPageTemplate = ({
   image,
   title,
